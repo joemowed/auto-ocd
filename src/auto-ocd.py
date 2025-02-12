ec7d8e3ad4742c85bc32f722160eba932ae3c6d8
joemowed <maloney.jg2@gmail.com>
2025-02-12 17:55:15 -0500

    install commands

--- src/auto-ocd.py
+++ src/auto-ocd.py
@@ -2,16 +2,19 @@
 import argparse
 import os
 import sys
+import shutil
 installDir = "/usr/share/auto-ocd"
 tmpDir = "/tmp/auto-ocd"
 
-def clrDir(path):
-
+def rmDir(path):
     try:
-        os.removedirs(path)
+        shutil.rmtree(path)
     except FileNotFoundError:
         pass
-        os.makedirs(path)
+
+def clrDir(path):
+    rmDir(path)
+    os.makedirs(path)
 
 if __name__ == "__main__": 
     parser = argparse.ArgumentParser()
@@ -22,13 +25,49 @@ if __name__ == "__main__":
 
     parser.add_argument("-g","--gdb",action="store_true",help="Creates openocd instance and calls gdb-multiarch on project executable.")
     parser.add_argument("--update",action="store_true",help=" Requires sudo.  Downloads newest version from github and adds \"auto-ocd.py\" to \"/usr/bin\" and copies nessasarry files to \"/usr/share/auto-ocd/*\". Overwrites existing open-ocd installation")
+    parser.add_argument("--update-local",action="store_true",help="Same as \"--update\", but uses the local src files instead of cloning from github.  Useful for development.")
     args = parser.parse_args()
     print(args)
-    if(args.update):
+    if(args.update or args.update_local):
         if not os.getenv("SUDO_USER"):
             print("Installation requries sudo privileges.")
             sys.exit()
-         
-        cloneCommand = f"git clone \"https://github.com/joemowed/auto-ocd\" {tmpDir}"
-        clrDir(tmpDir)
-        os.system(cloneCommand)
+        if args.update_local:
+            cwd = os.path.dirname(__file__)
+            if -1 == cwd.find("auto-ocd/src"):
+                print("Error:  Cannot run \"--update-local\" from the installed version of auto-ocd.  To update, try \"--update\" instead.")
+                sys.exit()
+            rmDir(tmpDir)
+            shutil.copytree(cwd,f"{tmpDir}/src")
+
+        else:   
+            print("Downloading latest version...")
+            clrDir(tmpDir)
+            cloneCommand = f"git clone \"https://github.com/joemowed/auto-ocd\" {tmpDir}"
+            os.system(cloneCommand)
+
+
+        print("Creating \"/usr/share/auto-ocd\"...")
+        clrDir(installDir)
+        copyCommand = f"cp {tmpDir}/src/* {installDir}/"
+        print("Copying dependencies...")
+        os.system(copyCommand)
+        copyBinCommand = f"cp {tmpDir}/src/auto-ocd.py /usr/bin"
+        print("Creating executable...")
+        try:
+            os.remove("/usr/bin/auto-ocd")
+        except FileNotFoundError:
+            pass
+        try:
+            os.remove("/usr/bin/auto-ocd.py")
+        except FileNotFoundError:
+            pass
+        os.system(copyBinCommand)
+        renameBinCommand = f"mv /usr/bin/auto-ocd.py /usr/bin/auto-ocd"
+        os.system(renameBinCommand)
+        print("Removing temporary files...")
+        rmDir(tmpDir)
+        green_text = "\033[32m"
+        reset_color = "\033[0m"
+
+        print(f"{green_text}auto-ocd Successfully Installed{reset_color}")
