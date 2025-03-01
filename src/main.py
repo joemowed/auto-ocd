e708cce07607f383a749c4ebb8f99c3f36522b8a
joemowed <maloney.jg2@gmail.com>
2025-03-01 12:54:52 -0500

    changed to work with bootlib

--- src/main.py
+++ src/main.py
@@ -29,9 +29,9 @@ class AutoOCD:
         os.system("mkdir -p build")
         os.system("cd ./build")
         if(self.args.release):
-            os.system("cmake -DCMAKE_BUILD_TYPE=Release -B./build ./CubeMX")
+            os.system("cmake -DCMAKE_BUILD_TYPE=Release -B./build ./device")
         else:
-            os.system("cmake -B./build ./CubeMX")
+            os.system("cmake -B./build ./device")
         os.system("ln -s cmake/compile_commands.json ../ > /dev/null 2>&1")
         ret = os.system("cd build && make")
         if ret != 0:
